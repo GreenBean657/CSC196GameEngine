c9e2ea4bea9b3f2698ab7ce1b395ab9a395c9100
GreenBean657 <greenbean657@proton.me>
2025-07-17 14:41:59 -0600

    Added a shape

--- source/game/shapedrawer.h
+++ source/game/shapedrawer.h
@@ -1,8 +1,48 @@
-//
-// Created by GreenBean on 7/17/25.
-//
+#pragma once
 
-#ifndef SHAPEDRAWER_H
-#define SHAPEDRAWER_H
+#include <bits/ostream.tcc>
 
-#endif //SHAPEDRAWER_H
+#include "../engine/include/math/vectors/vector2.h"
+#include "input/core.h"
+#include "renderer/core.h"
+
+#include <SDL3/SDL.h>
+
+#include "shapedrawer.h"
+#include "renderer/model.h"
+#include <iostream>
+using namespace bean_engine;
+inline int drawShape() {
+
+    bean_renderer::renderModule renderer("Shape Window");
+    bean_input::inputModule input;
+    bool quit = false;
+    while (!quit) {
+        quit = renderer.tryExit();
+        if (input.getKeyDown(SDL_SCANCODE_Q)) {
+            quit = true;
+        }
+        std::vector<bean_math::vector2<float>> vertices;
+        vertices.emplace_back(160, 220);
+        vertices.emplace_back(220, 180);
+        vertices.emplace_back(280, 220);
+        vertices.emplace_back(340, 180);
+        vertices.emplace_back(380, 190);
+        vertices.emplace_back(390, 240);
+        vertices.emplace_back(380, 280);
+        vertices.emplace_back(300, 320);
+        vertices.emplace_back(340, 260);
+        vertices.emplace_back(290, 255);
+        vertices.emplace_back(240, 320);
+        vertices.emplace_back(200, 270);
+        vertices.emplace_back(170, 270);
+        vertices.emplace_back(vertices[0]);
+        bean_renderer::model model(vertices, bean_math::color4(0, 255, 0, 0));
+        model.draw(renderer);
+
+        renderer.present();
+        input.tick();
+    }
+    std::cout << "Bye!" << std::endl;
+    return 0;
+}
