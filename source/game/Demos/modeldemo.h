96117427a9c1d555cdf6ce91221dd195473d2f7e
GreenBean657 <greenbean657@proton.me>
2025-07-29 19:53:59 -0600

    Made some adjustments

--- source/game/Demos/modeldemo.h
+++ source/game/Demos/modeldemo.h
@@ -30,13 +30,26 @@ inline int demo() {
     vertices.emplace_back(vertices[0]);
 
     const bean_renderer::model model(vertices, bean_math::color4(255, 255, 255));
-
+    bean_math::transform transform{bean_math::vector2<float>(0, 0), 180, 20};
     while (!quit) {
         quit = renderer.tryExit();
+        float speed = 200.0f;
 
+        bean_math::vector2<float> direction(0, 0);
+        if (input.getKeyDown(SDL_SCANCODE_W)) direction.y = -1;
+        if (input.getKeyDown(SDL_SCANCODE_S)) direction.y = 1;
+        if (input.getKeyDown(SDL_SCANCODE_A)) direction.x = -1;
+        if (input.getKeyDown(SDL_SCANCODE_D)) direction.x = 1;
+        if (direction.lengthSqr() > 0) {
+            direction = direction.normalized();
+            transform.position += (direction * speed) * time.getDeltaTime();
+        }
+
+        if (input.getKeyDown(SDL_SCANCODE_Q)) transform.rotation -= bean_math::degToRad(90 * time.getDeltaTime());
+        if (input.getKeyDown(SDL_SCANCODE_E)) transform.rotation += bean_math::degToRad(90 * time.getDeltaTime());
         renderer.setDrawColor(bean_math::color4(0, 0, 0, 255));
         renderer.clear();
-        model.Draw(renderer, input.getMousePosition(), time.getTime(), 10.0f);
+        model.Draw(renderer, transform);
         renderer.present();
         input.tick();
         time.tick();
